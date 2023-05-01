77dc0dd3936495a109ffc9242cc8068454ebbdb2
MorozovAlexanderr <sashamorozov309@gmail.com>
2023-05-01 18:55:14 +0300

    add rss feed parser

--- src/services/cron/tasks/parseRssFeed.ts
+++ src/services/cron/tasks/parseRssFeed.ts
@@ -1,4 +1,5 @@
 import Parser from 'rss-parser';
+import { PostModel } from '@/modules/post/post.model';
 
 const parser = new Parser();
 
@@ -6,8 +7,20 @@ const parseRssFeed = async () => {
   try {
     const feed = await parser.parseURL('https://lifehacker.com/rss');
 
-    feed.items.forEach((item) => {
-      console.log(item.guid);
+    feed.items.forEach(async (post) => {
+      const existedPost = await PostModel.findOne({ resourceId: post.guid });
+
+      if (existedPost) return;
+
+      await PostModel.create({
+        resourceId: post.guid,
+        title: post.title,
+        creator: post.creator,
+        body: post.content,
+        date: post.pubDate,
+      });
+
+      console.log(`New post added: ${post.title}`);
     });
   } catch (error) {
     console.log(error);
